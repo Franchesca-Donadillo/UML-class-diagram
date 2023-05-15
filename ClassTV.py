d376f82bd544ce33e63bf3f12b274d899b1d51cc
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-15 15:03:44 +0800

    not sure if working

--- ClassTV.py
+++ ClassTV.py
@@ -1,14 +1,37 @@
 # assign class
 class TV():
+
     # define start
     def Start(self):
         start_tv = input("WELCOME! Press any key to start. ")
         return start_tv
     
-    # define ask user
-    def AskUser():
-        channel = int(input("Enter your desired channel: "))
-        volume_level = int(input("Enter your desired level of volume: "))
-        on = bool(input("Is the TV on? "))
+    # constructor
+    def __init__(self, channel, volume_level, on)
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on = on
+    
+    def GetChannel(self):
+        return int(self.channel)
+    
+    def SetChannel(self, channel):
+        self.channel = channel
 
-        return channel, volume_level, on
+    def GetVolume(self):
+        return int(self.volume_level)
+    
+    def SetVolume(self, volume_level):
+        self.volume_level = volume_level
+    
+    def TurnOn(self):
+        return self.on
+    
+    def TurnOff(self):
+        return self.on
+    
+
+    
+    
+
+        
--- TestTV.py
+++ TestTV.py
@@ -5,3 +5,11 @@ from ClassTV import TV
 
 television = TV()
 television.Start()
+
+while True:
+    ask_user = television.AskUser()
+
+    # create tv_1
+    tv_1 = TV()
+    tv_1.channel = channel
+    print(tv_1.channel)
